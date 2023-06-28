38c86bc0e70adc3c48b99d76a596e79e29677b95
doyeolKR <86040882+doyeolKR@users.noreply.github.com>
2023-06-28 23:52:58 +0900

    [level 1] Title: 소수 만들기, Time: 0.16 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    private boolean[] prime = new boolean[3001];
+
+    public int solution(int[] nums) {
+        setPrimeArr();
+        int count = 0;
+        for(int i = 0; i < nums.length - 2; i++) 
+            for(int j = i + 1; j < nums.length - 1; j++) 
+                for(int k = j + 1; k < nums.length; k++) 
+                    if (!prime[nums[i] + nums[j] + nums[k]]) count++;
+
+        return count;
+    }
+
+    private void setPrimeArr() {
+        for(int i = 2; i * i <= 3000; i++)
+            for(int j = i; i * j <= 3000; j++)
+                prime[i * j] = true;
+    }
+}
