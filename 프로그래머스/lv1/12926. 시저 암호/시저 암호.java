e2a30deb97d8add34747166453f7d96df222957e
doyeolKR <86040882+doyeolKR@users.noreply.github.com>
2023-06-29 09:32:07 +0900

    [level 1] Title: 시저 암호, Time: 1.86 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -1,28 +1,20 @@
 class Solution {
+    public char push(char c, int n) {
+        if(!Character.isAlphabetic(c)) return c;
+        
+        int offset = Character.isUpperCase(c) ? 'A' : 'a';
+        int position = c - offset;
+        position = (position + n) % 26;
+        return (char)(offset + position);
+    }
+    
     public String solution(String s, int n) {
-        String answer = "";
-        
-        int Znum = 'Z';
-        int znum = 'z';
-        
+        StringBuilder sb = new StringBuilder();
         char[] arr = s.toCharArray();
         
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i] >= 'A' && arr[i] <= 'Z') {
-                arr[i] = (char)(arr[i] + n);
-                if(arr[i] > Znum) {
-                    arr[i] = (char)(arr[i] - 26);
-                }
-            }
-            else if(arr[i] >= 'a' && arr[i] <= 'z') {
-                arr[i] = (char)(arr[i] + n);
-                if(arr[i] > znum) {
-                    arr[i] = (char)(arr[i] - 26);
-                }
-            }
-            answer += arr[i];
+        for(char c : arr) {
+            sb.append(push(c,n));
         }
-        
-        return answer;
+        return sb.toString();
     }
 }
