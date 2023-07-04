54b1c1a873ca6473ff5c601ddd96d9870fb0eef1
doyeolKR <86040882+doyeolKR@users.noreply.github.com>
2023-07-04 13:01:09 +0900

    [level 1] Title: 소수 찾기, Time: 36.65 ms, Memory: 68.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -1,16 +1,29 @@
 class Solution {
-    public boolean isPrime(int n) {
+    public static boolean[] prime;
+    
+    public void make_prime(int n) {
+        prime = new boolean[n+1];
+        
+        if(n < 2) return;
+        
+        prime[0] = prime[1] = true;
+        
         for(int i=2; i<=Math.sqrt(n); i++) {
-            if(n % i == 0) return false;
+            if(prime[i]) continue;
+            
+            for(int j=i*i; j<prime.length; j+=i) {
+                prime[j] = true;
+            }
         }
-        return true;
     }
     
     public int solution(int n) {
         int answer = 0;
         
+        make_prime(n);
+        
         for(int i=2; i<=n; i++) {
-            if(isPrime(i)) answer++;
+            if(!prime[i]) answer++;
         }
         
         return answer;
