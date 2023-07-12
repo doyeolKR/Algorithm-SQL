a9171266c8d19e60adbebed84e3d76af443eef0c
doyeolKR <86040882+doyeolKR@users.noreply.github.com>
2023-07-12 17:19:50 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.11 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,17 +1,5 @@
 class Solution {
     public boolean solution(String s) {
-        boolean answer = true;
-        
-        char[] arr = s.toCharArray();
-        
-        if(!(s.length()==4 || s.length()==6)) return answer = false;
-        
-        for(int i=0; i<arr.length; i++) {
-            int val = arr[i];
-            if(!(val<=57 && val>=48)) 
-                answer = false; 
-        }
-        
-        return answer;
+        return s.matches("[0-9]{4}||[0-9]{6}");
     }
 }
