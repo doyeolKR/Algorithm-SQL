cac9677968daa3552d6dd15ea7188e945751d5a3
doyeolKR <86040882+doyeolKR@users.noreply.github.com>
2023-07-17 01:41:00 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.07 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,7 +1,6 @@
 class Solution {
     static int[] arr1 = new int[10];
     static int[] arr2 = new int[10];
-    static int[] arr3 = new int[10];
     
     public void makeArray(int[] arr, String str) {
         for(int i=0; i<str.length(); i++) {
@@ -14,14 +13,9 @@ class Solution {
         makeArray(arr1, X);
         makeArray(arr2, Y);
         
-        for(int i=0; i<10; i++) {
-            if(arr1[i] <= arr2[i]) arr3[i] = arr1[i];
-            else arr3[i] = arr2[i];
-        }
-        
         StringBuilder sb = new StringBuilder();
         for(int i=9; i>=0; i--) {
-            sb.append(String.valueOf(i).repeat(arr3[i]));
+            sb.append(String.valueOf(i).repeat(Math.min(arr1[i], arr2[i])));
         }
         String answer = sb.toString();
         if(answer.equals("")) answer = "-1";
