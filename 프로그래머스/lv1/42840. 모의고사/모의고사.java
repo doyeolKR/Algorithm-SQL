211c7eb157a3dfa31beba87b57a50b7dbfad3366
doyeolKR <86040882+doyeolKR@users.noreply.github.com>
2023-07-19 23:33:03 +0900

    [level 1] Title: 모의고사, Time: 4.48 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -1,46 +1,38 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
+    private static final int[][] RULES = {
+        {1,2,3,4,5},
+        {2,1,2,3,2,4,2,5},
+        {3,3,1,1,2,2,4,4,5,5},
+    };
+    
+    private int getPicked(int person, int problem) {
+        int[] rule = RULES[person];
+        int index = problem % rule.length;
+        return rule[index];
+    }
+    
     public int[] solution(int[] answers) {
-        int[] supo1 = {1,2,3,4,5}; //5
-        int[] supo2 = {2,1,2,3,2,4,2,5}; //8
-        int[] supo3 = {3,3,1,1,2,2,4,4,5,5}; // 10
+        int[] corrects = new int[3];
+        int max = 0;
         
-        int supo1_cnt = 0;
-        int supo2_cnt = 0;
-        int supo3_cnt = 0;
-        
-        for(int i=0; i<answers.length; i++) {
-            if(answers[i] == supo1[i%5]) supo1_cnt++;
-            if(answers[i] == supo2[i%8]) supo2_cnt++;
-            if(answers[i] == supo3[i%10]) supo3_cnt++;
+        for(int problem = 0; problem < answers.length; problem++) {
+            int answer = answers[problem];
+            
+            for(int person = 0; person < 3; person++) {
+                int picked = getPicked(person, problem);
+                if(picked == answer) {
+                    if(++corrects[person] > max) 
+                        max = corrects[person];
+                }
+            }
         }
-        
-        int best = Math.max(supo1_cnt, Math.max(supo2_cnt,supo3_cnt));
-        
-        int size = 0;
-        
-        if(supo1_cnt == best) size++;
-        if(supo2_cnt == best) size++;
-        if(supo3_cnt == best) size++;
-        
-        int[] answer = new int[size];
-        
-        int index = 0;
-        
-        if(supo1_cnt == best) {
-            answer[index] = 1;
-            index++;
-        }
-        if(supo2_cnt == best) {
-            answer[index] = 2;
-            index++;
-        }
-        if(supo3_cnt == best) {
-            answer[index] = 3;
-            index++;
-        }
-                
-        return answer;
+        final int maxCorrects = max;
+        return IntStream.range(0,3)
+            .filter(i -> corrects[i] == maxCorrects)
+            .map(i -> i+1)
+            .toArray();
     }
 }
