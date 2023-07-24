627ccf5bc8bc5e17e68afddb86309f8e6cae2f9c
doyeolKR <86040882+doyeolKR@users.noreply.github.com>
2023-07-24 10:32:37 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,7 @@
+SELECT
+    animal_id, 
+    name, 
+    sex_upon_intake
+FROM animal_ins
+WHERE name like 'Lucy' or name like 'Ella' or name like 'Pickle' or 
+name like 'Rogan' or name like 'Sabrina' or name like 'Mitty'
