bd6a6474df58643865e21454b10067c1f28de7c4
doyeolKR <86040882+doyeolKR@users.noreply.github.com>
2023-07-24 12:29:18 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -2,7 +2,7 @@ SELECT
     b.animal_id,
     b.name
 FROM animal_ins a
-INNER JOIN animal_outs b
+LEFT JOIN animal_outs b
 ON a.animal_id = b.animal_id
 ORDER BY datediff(b.datetime, a.datetime) desc
 limit 2
