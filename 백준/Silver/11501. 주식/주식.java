1ea2197c8d7ce733dae15fed882dc86a5ceb34b9
doyeolKR <86040882+doyeolKR@users.noreply.github.com>
2023-08-02 19:29:15 +0900

    [Silver II] Title: 주식, Time: 1180 ms, Memory: 314224 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -4,31 +4,31 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int testCase = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		for (int tc = 0; tc < testCase; tc++) {
-			int cnt = Integer.parseInt(br.readLine());
-			long[] sto = new long[cnt];
-			long max = 0;
-			long ans=0;
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < cnt; i++) {
-				sto[i]=Integer.parseInt(st.nextToken());
-			}
-			
-			for (int i = 1; i <= cnt; i++) {
-				if(sto[cnt-i]>max) {
-					max = sto[cnt-i];
-				}else {
-					ans+=(max-sto[cnt-i]);
-				}
-			}
-			sb.append(ans+"\n");
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+        StringTokenizer stz;
+        StringBuilder sb = new StringBuilder();
+
+        for (int tc = 0; tc < testCase; tc++) {
+            int days = Integer.parseInt(br.readLine());
+            int[] price = new int[days];
+            int max = 0;
+            long ans = 0;
+            stz = new StringTokenizer(br.readLine());
+            for (int i = 0; i < days; i++) {
+                price[i]=Integer.parseInt(stz.nextToken());
+            }
+
+            for(int i = days - 1; i >= 0; i--) {
+                if(price[i] > max) {
+                    max = price[i];
+                } else {
+                    ans += max - price[i];
+                }
+            }
+            sb.append(ans).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
