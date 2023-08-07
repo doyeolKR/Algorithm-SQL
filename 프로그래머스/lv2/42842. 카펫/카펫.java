5b48cf463cba57ee5207b9302569e90d42040928
doyeolKR <86040882+doyeolKR@users.noreply.github.com>
2023-08-07 17:25:57 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,23 +1,15 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        
-        int yRow = 0;
-        int yCol = 0;
-        
-        for(int row = 1; row<5000; row++) {
-            for(int col=1; col<5000; col++) {
-                if(row * col == yellow && (row*2)+((col*2)+4)==brown) {
-                    yRow = row;
-                    yCol = col;
-                    break;
-                }
+        for(int width = 3; width <= 5000; width++) {
+            for(int height = 3; height <= width; height++) {
+                int boundary = (width + height - 2) * 2;
+                int center = width * height - boundary;
+                
+                if(brown == boundary && yellow == center) 
+                    return new int[]{width, height};
             }
         }
         
-        answer[0] = yRow+2;
-        answer[1] = yCol+2;
-        
-        return answer;
+        return null;
     }
 }
