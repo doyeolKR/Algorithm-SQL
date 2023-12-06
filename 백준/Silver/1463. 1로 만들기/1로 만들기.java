fa658d6ad7c30132ca88d5c0412c1ba68b20ea94
doyeolKR <86040882+doyeolKR@users.noreply.github.com>
2023-12-06 22:52:12 +0900

    [Silver III] Title: 1로 만들기, Time: 260 ms, Memory: 21592 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,37 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
-    static Integer[] dp;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
-
-        int N = sc.nextInt();
-
-        dp = new Integer[N+1];
-        dp[0] = dp[1] = 0;
-
-        System.out.println(recur(N));
-
-    }
-
-    public static int recur(int N) {
-
-        if(dp[N] == null) {
-            if(N % 6 == 0) {
-                dp[N] = Math.min(recur(N/2), Math.min(recur(N / 3), recur(N - 1))) + 1;
-            }
-            else if(N % 3 == 0) {
-                dp[N] = Math.min(recur(N/3), recur(N-1)) + 1;
-            }
-            else if(N % 2 == 0) {
-                dp[N] = Math.min(recur(N/2), recur(N-1)) + 1;
-            }
-            else {
-                dp[N] = recur(N-1) + 1;
-            }
-        }
-        return dp[N];
-    }
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int[] dp = new int[N+1];
+		
+		dp[1] = 0;
+		
+		for(int i=2; i<=N; i++) {
+			if(i%6==0) {
+				dp[i] = Math.min(dp[i-1]+1, Math.min(dp[i/2]+1, dp[i/3]+1));
+			}
+			else if(i%3==0) {
+				dp[i] = Math.min(dp[i-1]+1, dp[i/3]+1);
+			}
+			else if(i%2==0) {
+				dp[i] = Math.min(dp[i-1]+1, dp[i/2]+1);
+			}
+			else {
+				dp[i] = dp[i-1]+1;
+			}
+		}
+		
+		System.out.println(dp[N]);
+		
+	} // main
 }
