ea2ed3aa0bec67612e9d04b63355133cc29f96f8
doyeolKR <86040882+doyeolKR@users.noreply.github.com>
2024-01-06 18:47:46 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 644 ms, Memory: 58856 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -11,24 +11,16 @@ public class Main {
         int N = Integer.valueOf(stz.nextToken()); // 수의 개수
         int M = Integer.valueOf(stz.nextToken()); // 구간 개수
 
-        int[] A = new int[N+1];
         int[] S = new int[N+1];
 
         stz = new StringTokenizer(br.readLine());
 
-        // 배열 A[] 의 값 넣어주기
         for(int i = 1; i < N+1; i++) {
-            A[i] = Integer.valueOf(stz.nextToken());
-        }
-
-        // 합 배열 S[]의 값 넣어주기
-        for(int i = 1; i < N+1; i++) {
-            S[i] = S[i-1] + A[i];
+            S[i] = S[i-1] + Integer.valueOf(stz.nextToken());
         }
 
         StringBuilder sb = new StringBuilder();
 
-        // 구간 합 출력하기
         for(int i = 0; i < M; i++) {
             stz = new StringTokenizer(br.readLine());
             int a = Integer.valueOf(stz.nextToken());
