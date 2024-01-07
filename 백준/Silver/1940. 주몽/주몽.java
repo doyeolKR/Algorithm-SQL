7e7763d493ac5f98e367739ca0d047e7fc5e552c
doyeolKR <86040882+doyeolKR@users.noreply.github.com>
2024-01-07 15:56:51 +0900

    [Silver IV] Title: 주몽, Time: 208 ms, Memory: 16672 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,39 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stz;
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.valueOf(br.readLine());
+        int M = Integer.valueOf(br.readLine());
 
-        int n = Integer.parseInt(reader.readLine());
-        int m = Integer.parseInt(reader.readLine());
+        int[] A = new int[N+1];
+        stz = new StringTokenizer(br.readLine());
 
-        int[] material = new int[n];
-        StringTokenizer st = new StringTokenizer(reader.readLine());
-        for (int i=0; i<n; i++) {
-            material[i] = Integer.parseInt(st.nextToken());
+        for(int i = 1; i <=N; i++) {
+            A[i] = Integer.valueOf(stz.nextToken());
         }
+        Arrays.sort(A);
 
-        int cnt = 0;
-        for(int start=0; start<n; start++)  {
-            int sum = 0;
-            int end = start+1;
+        int start_index = 1, end_index = N, ans = 0;
 
-            while (end<n) {
-                sum = material[start];
-                sum += material[end++];
-                if (sum == m) {
-                    cnt++;
-                    break;
-                }
+        while(start_index < end_index) {
+            int sum = A[start_index] + A[end_index];
+
+            if(sum == M) {
+                ans++;
+                end_index--;
             }
-
+            else if(sum > M) {
+                end_index--;
+            }
+            else if(sum < M) {
+                start_index++;
+            }
         }
 
-        System.out.println(cnt);
-
+        System.out.print(ans);
     }
 }
