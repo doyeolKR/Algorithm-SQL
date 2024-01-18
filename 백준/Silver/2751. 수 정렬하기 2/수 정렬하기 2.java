a5d853a323ebef41fd9e46973c14bc8693a1929d
doyeolKR <86040882+doyeolKR@users.noreply.github.com>
2024-01-19 00:17:51 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 796 ms, Memory: 99252 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -3,57 +3,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void merge(int[] arr, int[] tmp, int start, int mid, int end) {
-        for(int i = start; i <= end; i++) {
-            tmp[i] = arr[i];
-        }
-        int point1 = start;
-        int point2 = mid + 1;
-        int index = start;
-
-        while(point1 <= mid && point2 <= end) {
-            if(tmp[point1] < tmp[point2]) {
-                arr[index] = tmp[point1];
-                point1++;
-            }
-            else {
-                arr[index] = tmp[point2];
-                point2++;
-            }
-            index++;
-        }
-        for(int i = 0; i <= mid - point1; i++) {
-            arr[index + i] = tmp[point1 + i];
-        }
-    }
-    public static void mergeSort(int[] arr, int[] tmp, int start, int end) {
-        if(start < end) {
-            int mid = (start + end) / 2;
-            mergeSort(arr, tmp, start, mid);
-            mergeSort(arr, tmp, mid + 1, end);
-            merge(arr, tmp, start, mid, end);
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.valueOf(br.readLine());
-        int[] arr = new int[N];
-        int[] tmp = new int[N];
+
+        int shift = 1000000;
+        int range = 2000001;
+
+        boolean[] arr = new boolean[range];
 
         for(int i = 0; i < N; i++) {
-            arr[i] = Integer.valueOf(br.readLine());
+            arr[Integer.valueOf(br.readLine()) + shift] = true;
         }
-
-        mergeSort(arr, tmp, 0, N-1);
 
         StringBuilder sb = new StringBuilder();
-
-        for(int num : arr) {
-            sb.append(num).append("\n");
+        for(int i = 0; i < range; i++) {
+            if(arr[i]) {
+                sb.append(i - shift).append("\n");
+            }
         }
-
         System.out.print(sb);
-
-    } // main
+    }
 }
