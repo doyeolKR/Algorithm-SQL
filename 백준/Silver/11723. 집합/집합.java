727c1139587919e7e0a704531faa38818ae6c2f7
doyeolKR <86040882+doyeolKR@users.noreply.github.com>
2024-05-20 10:24:44 +0900

    [Silver V] Title: 집합, Time: 1124 ms, Memory: 312992 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,51 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static Set<Integer> set = new HashSet<>();
-    private static StringBuilder sb = new StringBuilder();
+    public static Set<Integer> set = new HashSet<>();
 
-    private static void all() {
-        for(int i = 0; i < 20; i++) {
-            set.add(i + 1);
+    public static void all() {
+        for(int i = 1; i <= 20; i++) {
+            set.add(i);
         }
     }
 
-    private static void empty() {
-        for(int i = 0; i < 20; i++) {
-            set.remove(i + 1);
+    public static void empty() {
+        for(int i = 1; i <= 20; i++) {
+            set.remove(i);
         }
-    }
-
-    private static void check(int num) {
-        int val = set.contains(num) ? 1 : 0;
-        sb.append(val).append("\n");
-    }
-
-    private static void toggle(int num) {
-        if(set.contains(num)) set.remove(num);
-        else set.add(num);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int cnt = Integer.valueOf(br.readLine());
 
-        while(n-- > 0) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < cnt; i++) {
             StringTokenizer stz = new StringTokenizer(br.readLine());
-            String str  = stz.nextToken();
-            int num = 0;
-            if(!(str.equals("all") || str.equals("empty")))
-                num = Integer.parseInt(stz.nextToken());
 
-            if(str.equals("add")) set.add(num);
-            else if(str.equals("remove")) set.remove(num);
-            else if(str.equals("check")) check(num);
-            else if(str.equals("toggle")) toggle(num);
-            else if(str.equals("all")) all();
-            else if(str.equals("empty")) empty();
+            String command = stz.nextToken();
+
+            if(command.equals("add")) {
+                int num = Integer.valueOf(stz.nextToken());
+                set.add(num);
+            } else if(command.equals("remove")) {
+                int num = Integer.valueOf(stz.nextToken());
+                set.remove(num);
+            } else if(command.equals("check")) {
+                int num = Integer.valueOf(stz.nextToken());
+                if(set.contains(num))
+                    sb.append(1).append("\n");
+                else
+                    sb.append(0).append("\n");
+            } else if(command.equals("toggle")) {
+                int num = Integer.valueOf(stz.nextToken());
+                if(set.contains(num))
+                    set.remove(num);
+                else
+                    set.add(num);
+            } else if(command.equals("all")) {
+                all();
+            } else if(command.equals("empty")) {
+                empty();
+            }
         }
         System.out.println(sb);
     }
