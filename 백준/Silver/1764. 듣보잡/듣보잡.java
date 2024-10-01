29e76cab33ba93a98734007224eab2d863275c08
doyeolKR <86040882+doyeolKR@users.noreply.github.com>
2024-10-01 20:38:42 +0900

    [Silver IV] Title: 듣보잡, Time: 240 ms, Memory: 26348 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -12,37 +12,28 @@ public class Main {
         int N = Integer.parseInt(stz.nextToken());
         int M = Integer.parseInt(stz.nextToken());
 
-        Map<String, Integer> map = new HashMap<>();
+        Set<String> set  = new HashSet<>();
 
-        for(int i = 0; i < N; i++) {
-            map.put(br.readLine(), 1);
+        for(int i = 1; i <= N; i++) {
+            set.add(br.readLine());
         }
-
-        for(int i = 0; i < M; i++) {
-            String name = br.readLine();
-
-            if(map.containsKey(name)) {
-                map.put(name, map.get(name) + 1);
-            }
-        }
-
-        int cnt = 0;
 
         List<String> list = new ArrayList<>();
 
-        for(Map.Entry<String, Integer> entry : map.entrySet()) {
-            if (entry.getValue() >= 2) {
-                cnt++;
-                list.add(entry.getKey());
+        for(int i = 1; i <= M; i++) {
+            String name = br.readLine();
+
+            if(set.contains(name)) {
+                list.add(name);
             }
         }
 
+        sb.append(list.size()).append("\n");
+
         Collections.sort(list);
 
-        sb.append(cnt).append("\n");
-
-        for(String s : list) {
-            sb.append(s).append("\n");
+        for(String name : list) {
+            sb.append(name).append("\n");
         }
 
         System.out.print(sb);
