5306b9bb5fc0ea059caa76d184ad00b86e205060
doyeolKR <86040882+doyeolKR@users.noreply.github.com>
2024-10-05 16:29:40 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 884 ms, Memory: 90200 KB -BaekjoonHub

--- 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -1,67 +1,73 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int N, M, R, seq;
-    static ArrayList<ArrayList<Integer>> nodeList = new ArrayList<>();
-    static int[] order;
+    static int N, M, R, cnt;
+    static int[] ans;
     static boolean[] visited;
+    static List<List<Integer>> graph = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();
+
+    public static void dfs(int root) {
+        visited[root] = true;
+        ans[root] = ++cnt;
+
+        List<Integer> list = graph.get(root);
+        for (int num : list) {
+            if(!visited[num]) {
+                dfs(num);
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer stz = new StringTokenizer(br.readLine());
 
-        N = Integer.valueOf(stz.nextToken());
-        M = Integer.valueOf(stz.nextToken());
-        R = Integer.valueOf(stz.nextToken());
+        N = Integer.parseInt(stz.nextToken());
+        M = Integer.parseInt(stz.nextToken());
+        R = Integer.parseInt(stz.nextToken());
 
-        order = new int[N+1];
-        visited = new boolean[N+1];
+        ans = new int[N];
+        visited = new boolean[N];
 
-        for(int i = 0; i < N+1; i++) {
-            nodeList.add(new ArrayList<>());
+        for(int i = 1; i <= N; i++) {
+            graph.add(new ArrayList<>());
         }
 
         for(int i = 0; i < M; i++) {
             stz = new StringTokenizer(br.readLine());
-            int u = Integer.valueOf(stz.nextToken());
-            int v = Integer.valueOf(stz.nextToken());
 
-            nodeList.get(u).add(v);
-            nodeList.get(v).add(u);
+            int a = Integer.parseInt(stz.nextToken()) - 1;
+            int b = Integer.parseInt(stz.nextToken()) - 1;
+
+            graph.get(a).add(b);
+            graph.get(b).add(a);
         }
 
-        for(int i = 0; i < N+1; i++) {
-            Collections.sort(nodeList.get(i), Comparator.reverseOrder());
+        for(List<Integer> list : graph) {
+            Collections.sort(list, Collections.reverseOrder());
         }
 
-        seq = 1;
-        dfs(R);
+        dfs(R-1);
 
-        StringBuilder sb = new StringBuilder();
-        for(int i = 1; i < order.length; i++) {
-            sb.append(order[i]).append("\n");
+        for(int num : ans) {
+            sb.append(num).append("\n");
         }
 
         System.out.print(sb);
-    }
-
-    static void dfs(int node) {
-        visited[node] = true;
-        order[node] = seq++;
-
-        for(int i = 0; i < nodeList.get(node).size(); i++) {
-            int next = nodeList.get(node).get(i);
-            if(!visited[next]) {
-                dfs(next);
-            }
-        }
-
 
     }
 }
+
+
+
+
+
+
+
+
+
