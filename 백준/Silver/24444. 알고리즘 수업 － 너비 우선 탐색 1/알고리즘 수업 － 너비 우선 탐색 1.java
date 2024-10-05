9137253b1b598243f0986afe9b3ac6125cf17a33
doyeolKR <86040882+doyeolKR@users.noreply.github.com>
2024-10-05 18:20:38 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 968 ms, Memory: 91620 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N, M, R, cnt;
+    static int[] ans;
+    static boolean[] visited;
+    static List<List<Integer>> graph = new ArrayList<>();
+    static Queue<Integer> queue = new LinkedList<>();
+    static StringBuilder sb = new StringBuilder();
+
+    public static void bfs(int root) {
+        visited[root] = true;
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+            int currentNode = queue.poll();
+            ans[currentNode] = ++cnt;
+
+            for(int neighbor : graph.get(currentNode)) {
+                if(!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer stz = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(stz.nextToken());
+        M = Integer.parseInt(stz.nextToken());
+        R = Integer.parseInt(stz.nextToken());
+
+        ans = new int[N];
+        visited = new boolean[N];
+
+        for(int i = 1; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < M; i++) {
+            stz = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(stz.nextToken()) - 1;
+            int b = Integer.parseInt(stz.nextToken()) - 1;
+
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        for(List<Integer> list : graph) {
+            Collections.sort(list);
+        }
+
+        bfs(R-1);
+
+        for(int num : ans) {
+            sb.append(num).append("\n");
+        }
+
+        System.out.print(sb);
+
+    }
+}
