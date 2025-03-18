19a011d3cf833dd5b61931c957e81325b04968ee
doyeolKR <86040882+doyeolKR@users.noreply.github.com>
2025-03-18 10:17:14 +0900

    [Gold V] Title: 토마토, Time: 760 ms, Memory: 148268 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,74 +1,101 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-    static int N, M, H;
-    static int[][][] tomato;
-    static Queue<int[]> q = new LinkedList<>();
-    static int[] dh = {0, 0, 0, 0, -1, 1};
-    static int[] dy = {0, 0, -1, 1, 0, 0};
-    static int[] dx = {-1, 1, 0, 0, 0 ,0};
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int width, height, floor;
+    static int[][][] box;
+    static boolean[][][] visited;
+    static int[] dy = {-1, 1, 0, 0, 0, 0};
+    static int[] dx = {0, 0, -1, 1, 0 ,0};
+    static int[] dz = {0, 0, 0, 0, -1, 1};
+    static Queue<int[]> queue = new LinkedList<>();
 
-    public static void bfs() {
-        while(!q.isEmpty()) {
-            int[] current = q.poll();
+    static boolean setBox() throws IOException {
+        boolean isAllTomatoOk = true;
+
+        for(int depth = 0; depth < floor; depth++) {
+            for(int row = 0; row < height; row++) {
+                String[] input = br.readLine().split(" ");
+                for(int col = 0; col < width; col++) {
+                    box[depth][row][col] = Integer.parseInt(input[col]);
+                    if(box[depth][row][col] == 0) {
+                        isAllTomatoOk = false;
+                    } else if(box[depth][row][col] == 1) {
+                        queue.add(new int[]{depth, row, col});
+                        visited[depth][row][col] = true;
+                    }
+                }
+            }
+        }
+        return isAllTomatoOk;
+    }
+
+    static boolean validatePoint(int nd, int nr, int nc) {
+        return nd >= 0 && nd < floor && nr >= 0 && nr < height && nc >= 0 && nc < width;
+    }
+
+    static void bfs() {
+        while(!queue.isEmpty()) {
+            int[] currentPoint = queue.poll();
+            int depth = currentPoint[0];
+            int row = currentPoint[1];
+            int col = currentPoint[2];
 
             for(int i = 0; i < 6; i++) {
-                int[] next = {current[0] + dh[i], current[1] + dy[i], current[2] + dx[i]};
+                int nd = depth + dz[i];
+                int nr = row + dy[i];
+                int nc = col + dx[i];
 
-                if(next[0] < 0 || next[0] >= H || next[1] < 0 || next[1] >= N ||
-                    next[2] < 0 || next[2] >= M || tomato[next[0]][next[1]][next[2]] != 0)
-                    continue;
-
-                q.add(next);
-                tomato[next[0]][next[1]][next[2]] = tomato[current[0]][current[1]][current[2]] + 1;
+                if(validatePoint(nd, nr, nc) && !visited[nd][nr][nc] && box[nd][nr][nc] == 0) {
+                    queue.add(new int[]{nd, nr, nc});
+                    box[nd][nr][nc] = box[depth][row][col] + 1;
+                    visited[nd][nr][nc] = true;
+                }
             }
         }
     }
 
-    public static int whatDay() {
-        int day = 0;
+    static int checkBox() {
+        int lastDay = 0;
 
-        for(int i = 0; i < H; i++) {
-            for(int j = 0; j < N; j++) {
-                for(int k = 0; k < M; k++) {
-                    if(tomato[i][j][k] == 0)
+        for(int depth = 0; depth < floor; depth++) {
+            for(int row = 0; row < height; row++) {
+                for(int col = 0; col < width; col++) {
+                    if(box[depth][row][col] == 0) {
                         return -1;
-                    day = Math.max(day, tomato[i][j][k]);
+                    }
+                    if(box[depth][row][col] > lastDay) {
+                        lastDay = box[depth][row][col];
+                    }
                 }
             }
         }
-        return day - 1;
+        return lastDay - 1;
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer stz = new StringTokenizer(br.readLine());
 
-        M = Integer.parseInt(stz.nextToken());
-        N = Integer.parseInt(stz.nextToken());
-        H = Integer.parseInt(stz.nextToken());
+        String[] input = br.readLine().split(" ");
 
-        tomato = new int[H][N][M];
+        width = Integer.parseInt(input[0]);
+        height = Integer.parseInt(input[1]);
+        floor = Integer.parseInt(input[2]);
 
-        for(int i = 0; i < H; i++) {
-            for(int j = 0; j < N; j++) {
-                stz = new StringTokenizer(br.readLine());
-                for(int k = 0; k < M; k++) {
-                    tomato[i][j][k] = Integer.parseInt(stz.nextToken());
+        box = new int[floor][height][width];
+        visited = new boolean[floor][height][width];
 
-                    if(tomato[i][j][k] == 1)
-                        q.offer(new int[]{i, j, k});
-                }
-            }
+        boolean isAllTomatoOk = setBox();
+        if (isAllTomatoOk) {
+            System.out.println(0);
+            System.exit(0);
         }
 
         bfs();
 
-        System.out.print(whatDay());
-
+        System.out.println(checkBox());
     }
 }
